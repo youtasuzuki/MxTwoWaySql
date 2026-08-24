287f4f2a2b1a0d28b9dcf1bec34d8facb0edb9d7
youta suzuki <suzuki.y@buildsystem.jp>
2026-08-24 14:29:24 +0900

    ログレベルに関するヒントを表示するように変更

--- src/MxTwoWaySql/resources/sql/CopyToDeployment.bat
+++ src/MxTwoWaySql/resources/sql/CopyToDeployment.bat
@@ -1,2 +1,7 @@
+echo off
+echo --------------------------------------------------------------
+echo If the TwoWaySql log level is set to `debug` or `trace`,
+echo  changes to the copied SQL file will take effect immediately.
+echo --------------------------------------------------------------
 xcopy ..\sql ..\..\deployment\model\resources\sql /s /d /y
 pause
